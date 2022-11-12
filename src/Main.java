c23402e7cc514ce407723a7e882f7dca42cf75e5
Arihin <arihin.slava@yandex.ru>
2022-11-12 18:49:15 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Main {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < generateRandomArray().length; i++) {
+            if (i == generateRandomArray().length - 1)
+                System.out.println("Сумма трат за месяц составила " + IntStream.of(generateRandomArray()).sum() + " рублей ");
+            
+        }
     }
 }
